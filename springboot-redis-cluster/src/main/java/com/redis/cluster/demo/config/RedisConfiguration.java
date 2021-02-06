ecfe5af590941ef85be1997b71720df1ff77230e
Felicia <359468250@qq.com>
2021-02-06 23:30:14 +0800

    重写redis序列化

---
+++ springboot-redis-cluster/src/main/java/com/redis/cluster/demo/config/RedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.redis.cluster.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+    /**
+     * 重写Redis序列化方式，使用json方式；
+     * 让我们的数据存储到redis的时候，我们的key和value都是通过serializer序列化的到数据库的
+     * @param redisConnectionFactory
+     * @return
+     */
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
